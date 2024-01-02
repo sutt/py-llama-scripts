c7141f2d1687935ecf55c433ab3ab108033b4ba0
Will Sutton <wsutton17@gmail.com>
2024-01-01 19:46:16 -0500

    moving app to github.com/sutt/lime

--- eval-pipeline-1/scripts/agg.bat
+++
@@ -1,6 +0,0 @@
-
-python agg.py ^
-    -i ../wordle-qa-2/_results ^
-    -v 
-    
-@REM -d
--- eval-pipeline-1/scripts/launch.sh
+++
@@ -1,5 +0,0 @@
-python main.py \
-    -f ../wordle-qa-1/alpha/input-basic.md \
-    -u 2 \
-    -v 1 \
-    -j
